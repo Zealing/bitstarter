fd0776dc1ecba50040432b2ef1f6acf2437e0148
Zealing <vermilion.lin@gmail.com>
2015-12-11 05:24:48 +0000

    asdfasdf

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 var fs = require('fs');
-fs.readFileSync("index.html", function(err) {
-    if(err) {
-	return console.log(err);
-    }
-});
 
 app.get('/', function(request, response) {
     var fs = require('fs');
